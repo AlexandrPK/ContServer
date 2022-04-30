fa45551ffc8608ec6e7bed2ba42847cebfecd1d0
ALexandrPK <usu99@mail.ru>
2022-04-30 21:30:36 +0300

    Added two pages:auth,regist

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,94 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Form, Input, Button } from "antd";
+
+import { message } from "antd";
+// import { nullErrorMessage } from '../redux/authSlice'
+// import { useSelector } from 'react-redux'
+
+// import { loginThunk } from '../redux/authSlice'
+// import styles from '../styles/LoginForm.module.scss'
+
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 8,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
 
 const Login = () => {
+  // const dispatch = useDispatch()
+  // let errMessage = useSelector((state) => state.auth.errMessage)
+
+  // const onFinish = (values) => {
+  //   dispatch(loginThunk(values));
+  // };
+
+  const onFinishFailed = (errorInfo) => {};
+
+  // useEffect(() => {
+  //   errMessage &&
+  //     message.error(errMessage, () => dispatch(nullErrorMessage()), [
+  //       errMessage,
+  //     ]);
+  // });
+
   return (
-    <div>
-      <h1 style={{ margin: "15px", display: "flex", justifyContent: "center" }}>
-        Формочка Login
-      </h1>
-    </div>
+    <>
+
+      <h1 style={{ textAlign:"center", marginTop:"15px" }}>Вход</h1>
+      <Form
+        {...layout}
+        name="basic"
+        initialValues={{
+          remember: true,
+        }}
+        // onFinish={onFinish}
+        // onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            {
+              required: true,
+              type: "email",
+              message: "Введите действительный адрес электронной почты",
+            },
+          ]}
+        >
+          <Input  />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,15}$/,
+              message:
+                "Пароль должен содержать цифры, прописные и строчные буквы, длиной 8-15 символов",
+            },
+          ]}
+        >
+          <Input.Password  />
+        </Form.Item>
+
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            Войти
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
   );
 };
 
--- src/pages/Regist.jsx
+++ src/pages/Regist.jsx
@@ -1,22 +1,117 @@
 import React from "react";
+import { Form, Input, Button } from "antd";
+// import { useDispatch } from 'react-redux'
 
-const Regist = () => {
+// import { registerThunk } from '../redux/registerSlice'
+// import styles from '../styles/RegisterForm.module.scss'
+
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 8,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
+
+const RegisterForm = () => {
+  // const dispatch = useDispatch()
+
+  // const onFinish = (values) => {
+  //   dispatch(registerThunk(values))
+  // }
+
+  // const onFinishFailed = (errorInfo) => {}
+
   return (
-    <div>
-      <h1
-        style={{
-          margin: "15px",
-          display: "flex",
-          justifyContent: "center",
-          textAlign: "center",
+    <>
+      <h1 style={{ textAlign: "center", marginTop: "15px" }}>Регистрация</h1>
+      <Form
+        {...layout}
+        name="basic"
+        initialValues={{
+          remember: true,
         }}
+        // onFinish={onFinish}
+        // onFinishFailed={onFinishFailed}
       >
-        Нужно зарегестриваться, а то, как не родной
-        <br />
-        формочка
-      </h1>
-    </div>
+        <Form.Item
+          label="Имя"
+          name="firstname"
+          rules={[{ required: true, message: "Введите имя" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Фамилия"
+          name="lastname"
+          rules={[{ required: true, message: "Введите фамилию" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Отчество"
+          name="middlename"
+          rules={[{ required: true, message: "Введите отчество" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            {
+              required: true,
+              type: "email",
+              message: "Введите действительный адрес электронной почты",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            {
+              required: true,
+              type: "email",
+              message: "Введите действительный адрес электронной почты",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,15}$/,
+              message:
+                "Пароль должен содержать цифры, прописные и строчные буквы, длиной 8-15 символов",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            Pарегистрироваться
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
   );
 };
 
-export default Regist;
+export default RegisterForm;
